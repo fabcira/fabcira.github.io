6e2ffe18fa58290531acdb66b49b9c36c8d9c9d2
“Fabio <f.ciravegna@shef.ac.uk>
2018-02-06 22:29:58 +0000

    first server worker

--- js/main.js
+++ js/main.js
@@ -9,3 +9,21 @@ if ('serviceWorker' in navigator) {
     });
   });
 }
+
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/styles/main.css',
+  '/script/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
